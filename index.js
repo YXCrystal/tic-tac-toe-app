6a4359904d9918feabfa22633677437da5f97631
Crystal <yxcwuu@gmail.com>
2020-04-21 15:21:05 -0400

    Add win conditions

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const displayController = (function() {
     let playerBtn = document.querySelector("#player_btn");
     let player1 = document.querySelector("#p1");
     let player2 = document.querySelector("#p2");
+    let gridItems = document.querySelectorAll(".grid-item");
     let symbol = "O";
 
     function changeName() {
@@ -41,8 +42,9 @@ const displayController = (function() {
     function newGame() {
         p1Name.style.display = "block";
         p2Name.style.display = "block";
-        playerBtn.textContent = "Start Game"
-        
+        playerBtn.textContent = "Start Game";
+        gridItems.forEach((item) => item.textContent = String.fromCharCode(160));
+        symbol = "O"
     }
 
     return {startGame, newGame, playSymbol}
@@ -64,8 +66,6 @@ const gameController = (function() {
     function addPlayers() {
         let p1 = Player(p1Name, "X");
         let p2 = Player(p2Name, "O");
-
-        return {p1, p2}
     };
 
     function startGame() {
@@ -75,32 +75,34 @@ const gameController = (function() {
             if (start) {
                 start = false;
                 addPlayers();
-                console.log(addPlayers().p1.name);
-                console.log(addPlayers().p2.name);
-
                 displayController.startGame();
+                grid.style.display = "grid";
             } else {
                 start = true;
+                win = false;
                 displayController.newGame();
+                grid.style.display = "none";
+                player1 = [];
+                player2 = [];
+                player = 1;
             }
-            grid.style.display = "grid";
         }));
     }
 
     function playGame(item) {
-
         if (player == 1) {
-            player1.push(parseInt(item.id))
-            winCondition(player1)
             if (!win) {
                 player = 2
             }
+            player1.push(parseInt(item.id));
+            winCondition(player1);
+            
         } else {
-            player2.push(parseInt(item.id));
-            winCondition(player2)
             if (!win) {
                 player = 1
             }
+            player2.push(parseInt(item.id));
+            winCondition(player2);
         }
     }
 
@@ -108,20 +110,19 @@ const gameController = (function() {
     function winCondition(player) {
         let board = [];
         let winCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], 
-                        [1, 4, 7], [2, 5, 8], [3, 6, 9], 
-                        [1, 5, 9], [3, 5, 7]];
+                        [1, 4, 7], [2, 5, 8], [3, 5, 7], [3, 6, 9], 
+                        [1, 5, 9]];
 
         for (var i = 0; winCombos.length > i; i++) {
-            if (board.length != 3) {
-                board = winCombos[i].filter((e) => player.includes(e))
-            } else {
+            if (board.length == 3) {
                 win = true;
-            }
+            } else {
+                board = winCombos[i].filter((e) => player.includes(e))
+            }   
         }
     }
 
     function gameBoard() {
-        
         let gridItems = document.querySelectorAll(".grid-item");
         gridItems.forEach(function(item) {
                 item.addEventListener("click", (function() {
@@ -131,8 +132,9 @@ const gameController = (function() {
                             displayController.playSymbol(item);
                         }
                     } else if (win == true) {
-                        let winner = 
-                        alert(`Player ${player} Won!`)
+                        let winner = "";
+                        (player == 1? winner = p2Name : winner = p1Name);
+                        alert(`${winner} Won!`);
                     }
                 }));
             });
